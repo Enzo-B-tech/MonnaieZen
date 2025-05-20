d7e71ef240f2504b08985a881bfc61402730d379
0wl3ssY <ebouam@turgot-paris.info>
2025-05-20 17:22:17 +0200

    Delete jest.config.cjs

--- jest.config.cjs
+++
@@ -1,5 +0,0 @@
-// jest.config.cjs
-module.exports = {
-  testEnvironment: 'node', // utilise Node.js pour les tests
-  transform: {},           // pas de transformateurs, on s'appuie sur ESM natif
-};
